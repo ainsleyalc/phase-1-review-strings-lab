0ffd57fdee8133b9b054bae78df221b416d32d4d
Ainsley Alceme <youngchill101@gmail.com>
2023-03-20 16:08:55 -0400

    submits

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "GRACIE";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
+let shortGreeting = `Welcome, ` + currentUser.slice(0,1) + '!';
